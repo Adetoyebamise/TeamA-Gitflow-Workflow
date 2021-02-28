7292dab9c9ce33cf2d5d5c7025729d2703542e14
vwede oteghele <louisshaw75@gmail.com>
2021-02-28 20:26:39 +0100

    assigment

---
+++ hashassigment.sh
@@ -0,0 +1,11 @@
+#!/bin/env bash
+
+sha1sum mondayboard.jpeg > shacodes.txt 
+
+sha1sum tuesdayboard.jpeg >> shacodes.txt
+
+sha1sum wednesdayboard.jpeg >> shacodes.txt
+
+sha1sum thursdayboard.jpeg >> shacodes.txt
+
+sha1sum fridayboard.jpeg >> shacodes.txt
