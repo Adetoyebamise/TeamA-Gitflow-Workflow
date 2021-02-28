8bceca4bc470a939a7d1136d8caa219f1b5b4be2
adetoyebamise <adetoyebamise@gmail.com>
2021-02-28 23:40:01 +0100

    add hash code generation script file

---
+++ hashcodegeneration.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+sha1sum feb22.jpg >> hashcodegeneration.txt
+sha1sum feb23.jpg >> hashcodegeneration.txt
+sha1sum feb24.jpg >> hashcodegeneration.txt
+sha1sum feb25.jpg >> hashcodegeneration.txt
+sha1sum feb26.jpg >> hashcodegeneration.txt
