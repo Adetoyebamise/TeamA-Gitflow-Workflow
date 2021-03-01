05c06c1e905b3857ae1ca7b966bec8d1d4fdbf9e
Oluwasegun Ajayi <ajayimooseize@gmail.com>
2021-03-01 04:49:06 +0100

    This bash script generate a hash string for all the images in the array

---
+++ hashpic.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+
+#Generate a hash string for all week 6 board Images and store the file name and hash string inside a text file.
+
+#this script generates a md5 hash string for all images.
+
+function GetImages(){
+	FILES=`cd Week_6_Board_Images|ls -1|sort|head -5`
+}
+
+
+function Hashimages(){
+	local COUNT=1
+	for FILE in $@
+	do
+		md5sum $FILE >> array_md5_hash_strings.txt
+		((COUNT++))
+	done
+}
+
+GetImages
+Hashimages $FILES
+
+exit 0
